8c063bf56de8f834837d19df43540d95848d74db
Michael Maker <maker.mdm@gmail.com>
2022-03-21 17:50:22 -0500

    In progress on cityInput event handler

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Main extends React.Component {
 
@@ -6,32 +7,66 @@ class Main extends React.Component {
     super(props)
     this.state = {
       city: '',
-      cityList: {}
+      cityList: {},
+
+      // variable to store data
+      starWarsData:[],
+      cityData: {}
     }
   }
 
-  handleCity = (e) => {
-    this.setState({
-      city: e.target.value
-    })
-  }
+//   handleCityInput = (e) => {
+//     this.setState({
+//       city: e.target.value
+//     })
+//   }
+
+// // two keywords for axios
+//   handleStarWars = async(e) => {
+//     //get the data from the API
+//     // axios is a lightweight way to call method doing heavy lifting of api call
+//     e.preventDefault();
+//     let starWarsCharacters = await axios.get('https://swapi.dev/api/people/?page=1');
+//     console.log(starWarsCharacters);
+
+//     // save that data into state to use and t re-render page with the state change
+//     this.setState({
+//       starWarsData: starWarsCharacters.data.results
+//       // add in data on call when using axios
+//     });
+//   }
+
+
+
+//   getCityData = async(e) => {
+//     e.preventDefault();
+//     let cityData = await axios.get('ENTER THE URL YOU NEED HERE!!!!');
+//     console.log(cityData[0]);
+//   }
 
 
 
   render() {
     console.log('main props', this.props);
     console.log('main state', this.state);
+    let starWarsListItems = this.state.starWarsData.map((character,idx) => {
+     return <li key={idx}>{character.name}</li>
+    });
 
 
     return (
       <main>
-        <form>
+        <form onSubmit={this.getCityData}>
           <label>Pick a City
             {/* goal of next week is to have this button trigger an api call */}
-            <input type="text" onInput={this.handleCity} name="city" />
+            <input type="text" onInput={this.handleCityInput} name="city" />
           </label>
           <button type='submit'>Find City</button>
         </form>
+
+        <ul>
+          {starWarsListItems}
+        </ul>
       </main>
     )
   }
