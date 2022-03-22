9d2a05a00afb6d5e63a3144ff6e29825386d6ea5
Michael Maker <maker.mdm@gmail.com>
2022-03-21 21:56:14 -0500

    Delete state and event handler from main

--- src/Main.js
+++ src/Main.js
@@ -9,49 +9,19 @@ class Main extends React.Component {
       city: '',
       cityList: {},
 
-      // variable to store data
-      starWarsData:[],
-      cityData: {}
+
     }
   }
 
-//   handleCityInput = (e) => {
-//     this.setState({
-//       city: e.target.value
-//     })
-//   }
-
-// // two keywords for axios
-//   handleStarWars = async(e) => {
-//     //get the data from the API
-//     // axios is a lightweight way to call method doing heavy lifting of api call
-//     e.preventDefault();
-//     let starWarsCharacters = await axios.get('https://swapi.dev/api/people/?page=1');
-//     console.log(starWarsCharacters);
-
-//     // save that data into state to use and t re-render page with the state change
-//     this.setState({
-//       starWarsData: starWarsCharacters.data.results
-//       // add in data on call when using axios
-//     });
-//   }
-
-
-
-//   getCityData = async(e) => {
-//     e.preventDefault();
-//     let cityData = await axios.get('ENTER THE URL YOU NEED HERE!!!!');
-//     console.log(cityData[0]);
-//   }
+  
 
 
 
   render() {
     console.log('main props', this.props);
     console.log('main state', this.state);
-    let starWarsListItems = this.state.starWarsData.map((character,idx) => {
-     return <li key={idx}>{character.name}</li>
-    });
+
+    
 
 
     return (
@@ -61,7 +31,7 @@ class Main extends React.Component {
             {/* goal of next week is to have this button trigger an api call */}
             <input type="text" onInput={this.handleCityInput} name="city" />
           </label>
-          <button type='submit'>Find City</button>
+          <button type='submit'>Explore!</button>
         </form>
 
         <ul>
